e85689ae9187428921320f42319f80eb7d5c40fc
Axel Guerin <59256787+axelguerinoctave@users.noreply.github.com>
2021-10-07 11:16:25 +0200

    ajout test au tp 8

--- Complément Informatique/TP_8.py
+++ Complément Informatique/TP_8.py
@@ -2,23 +2,23 @@ def produit_scalaire_vecteur(l, k, compteur = []):
     if l == []:
         return compteur
     else:
-        compteur.append(l.pop(0) * k)
+        compteur.append(k * l.pop(0))
         return produit_scalaire_vecteur(l, k, compteur)
 
 def produit_scalaire_matrice(l, k, compteur = []):
     if l == []:
         return compteur
     else:
-        compteur.append(produit_scalaire_vecteur(l[0], k, []))
-        l.pop(0)
+        compteur.append(produit_scalaire_vecteur(l.pop(0), k, []))
         return produit_scalaire_matrice(l, k, compteur)
 
-l = [1, 2, 3]
-print(l)
-print(produit_scalaire_vecteur(l, 2))
+def Test():
+    l = [1, 2, 3]
+    print(l)
+    print(produit_scalaire_vecteur(l, 2))
 
+    l = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(l)
+    print(produit_scalaire_matrice(l, 2))
 
-l = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(l)
-print(produit_scalaire_matrice(l, 2))
-
+Test()
